a94adf73cb1d72fae63aa55ff280819fd5e5f821
JosseGonnza <accion.quevedo@hotmail.com>
2024-12-02 15:45:17 +0000

    🟢 green: test "when word contains a non repeated result is this character" has passed

--- LeanMindInterview/UnitTest1.cs
+++ LeanMindInterview/UnitTest1.cs
@@ -4,11 +4,11 @@ namespace LeanMindInterview;
 /*
  * "" -> ""
  *
- * "s" -> "s"
- * "*" -> "*"
  * "ss" -> ""
  * "**" -> ""
  * "sstt" -> ""
+ * "s" -> "s"
+ * "*" -> "*"
  *
  * "st" -> "s"
  * "*t" -> "*"
@@ -30,9 +30,33 @@ public class UnitTest1
 
         result.Should().Be("");
     }
+    
+    [Fact(DisplayName = "when word contains a non repeated result is this character")]
+    public void when_word_contains_a_non_repeated_result_is_this_character()
+    {
+        var word = "s";
 
+        var result = firstNonRepeatingLetter(word);
+
+        result.Should().Be("s");
+    }
+
+    // [Fact(DisplayName = "when word contains all characters repeated result is empty text")]
+    // public void when_word_contains_all_characters_repeated_result_is_empty_text()
+    // {
+    //     var word = "ss";
+    //
+    //     var result = firstNonRepeatingLetter(word);
+    //
+    //     result.Should().Be("");
+    // }
+    
     private string firstNonRepeatingLetter(string word)
     {
+        if (word.Length != 0)
+        {
+            return word;
+        }
         return "";
     }
 }
